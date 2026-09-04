44cf4cd841716a792c747a79ed4c5c1d068bc3d2
qtchini2ja93085-spec <qtchini2ja93085@gmail.com>
2026-09-05 05:16:12 +0800

    Add capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.epmsolutions.callconsole',
+  appName: 'CALLCONSOLE',
+  webDir: 'www'
+};
+
+export default config;
